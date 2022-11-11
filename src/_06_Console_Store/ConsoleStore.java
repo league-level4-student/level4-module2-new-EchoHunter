4217cf13308b143e7f2475066b99fcdcb1cf454d
Student <league@iMac-34.attlocal.net>
2022-11-10 18:14:22 -0800

    :b:

--- src/_06_Console_Store/ConsoleStore.java
+++ src/_06_Console_Store/ConsoleStore.java
@@ -3,68 +3,78 @@ package _06_Console_Store;
 import java.util.Iterator;
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class ConsoleStore {
 
-    /*
-     * Write a program that simulates shopping in a store using the Scanner and
-     * the classes in Generics_Store.
-     * 
-     * Note: You may need to modify existing code in Generics Store and/or add
-     * additional items and pictures to fulfill all the requirements. You are
-     * also free to add any additional methods or classes in Console Store that
-     * might be helpful to you.
-     * 
-     * Requirements:
-     * 
-     * -Use Ternary operators in place of simple if/else statements and do-while
-     * loops instead of while loops where appropriate.
-     * 
-     * - There should be at least four unique items the user can buy. These can
-     * be food items, nonfood items or both.
-     * 
-     * - The user should have a stipend of money to spend and each item should
-     * have its own price.
-     * 
-     * -The user should have the ability to add items to their cart, remove
-     * items, view items or check out.
-     * 
-     * -The program should continue until the user chooses to check out.
-     * 
-     * -When the user checks out you should let them know if they do not have
-     * enough money to purchase all their items and offer to put items back.
-     * 
-     * -If the user successfully purchases the items you should remove the
-     * amount from their stipend, show them the pictures of what they bought and
-     * print out a receipt showing their name, the individual prices of the
-     * items and their total.
-     */
+	/*
+	 * Write a program that simulates shopping in a store using theNum Scanner and
+	 * theNum classes in Generics_Store.
+	 * 
+	 * Note: You may need to modify existing code in Generics Store and/or add
+	 * additional items and pictures to fulfill all theNum requirements. You are
+	 * also free to add any additional methods or classes in Console Store that
+	 * might be helpful to you.
+	 * 
+	 * Requirements:
+	 * 
+	 * -Use Ternary operators in place of simple if/else statements and do-while
+	 * loops instead of while loops where appropriate.
+	 * 
+	 * - theNumre should be at least four unique items theNum user can buy. theNumse
+	 * can be food items, nonfood items or both.
+	 * 
+	 * - theNum user should have a stipend of money to spend and each item should
+	 * have its own price.
+	 * 
+	 * -theNum user should have theNum ability to add items to theNumir cart, remove
+	 * items, view items or check out.
+	 * 
+	 * -theNum program should continue until theNum user chooses to check out.
+	 * 
+	 * -When theNum user checks out you should let theNumm know if theNumy do not
+	 * have enough money to purchase all theNumir items and offer to put items back.
+	 * 
+	 * -If theNum user successfully purchases theNum items you should remove theNum
+	 * amount from theNumir stipend, show theNumm theNum pictures of what theNumy
+	 * bought and print out a receipt showing theNumir name, theNum individual
+	 * prices of theNum items and theNumir total.
+	 */
 
-    public static void main(String[] args) {
-    	Cart<Food> c = new Cart<Food>();
-    	double money = 24.99;
-    	Scanner s = new Scanner(System.in);
-    	String the = "";
-    	boolean stillShop = true;
-    	do {
-    		System.out.println("You are at the gorcery store, what would you like to buy? \nCereal - $4.99 \nCandy - $0.99 \nFancyCheese - $13.99\nAvocado - $1.99 ");
-    	 the = s.next() == "Cereal" ? "4.99": s.next();
-    	 the = the == "Candy" ? "0.99": s.next();
-    	 the = the == "FancyCheese" ? "13.99": s.next();
-    	 the = the == "Avocado" ? "1.99": s.next();
-    	 money -= Double.parseDouble(the);
-    	 if (the.equals("Cereal")) {
-			c.add(new Cereal());
-		} else if (the.equals("Candy")) {
-			c.add(new Candy());
-		}else if (the.equals("FancyCheese")) {
-			c.add(new FancyCheese());
-		}else if (the.equals("Avocado")) {
-			c.add(new Avocado());
-		}
-    	 	System.out.println("Would you like to continue shopping? true/false");
-    	 	stillShop = s.nextBoolean();
-    	}while(stillShop);
-    	s.close();
-    }
+	public static void main(String[] args) {
+		Cart<Food> c = new Cart<Food>();
+		int money = 25;
+		Scanner s = new Scanner(System.in);
+		int theNum = 0;
+		boolean stillShop = true;
+		do {
+			System.out.println(
+					"You are at the grocery store, what would you like to buy? \n1:Cereal - $5 \n2:Candy - $1 \n3:FancyCheese - $14\n4:Avocado - $2 ");
+			theNum = s.nextInt();
+//			theNum = theNum == 1 ? 5 : theNum;
+//			theNum = theNum == 2 ? 1 : theNum;
+//			theNum = theNum == 3 ? 14 : theNum;
+//			theNum = theNum == 4 ? 2 : theNum;
+//			money -= theNum;
+			if (theNum == 1) {
+				c.add(new Cereal());
+				money -= 5;
+			} else if (theNum == 2) {
+				c.add(new Candy());
+				money -= 1;
+			} else if (theNum == 3) {
+				c.add(new FancyCheese());
+				money -= 14;
+			} else if (theNum == 4) {
+				c.add(new Avocado());
+				money -= 2;
+			}
+			System.out.println("Would you like to continue shopping? true/false");
+			stillShop = s.nextBoolean();
+		} while (stillShop);
+		s.close();
+		JOptionPane.showMessageDialog(null, "You spent " + (25-money) + " and have " + money + " leftover.\nHeres your cart.");
+		c.showCart();
+	}
 
 }
